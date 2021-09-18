c82ea57955ae06486dc52242189194d5f968b610
Triin Järv <trjarv@tktk.ee>
2021-09-18 15:23:31 +0300

    add new projects

---
+++ CatsAndDogs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CatsAndDogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you more like a cat or adog?");
+            string userChoise = Console.ReadLine().ToLower();
+
+            if (userChoise == "cat") 
+            {
+                Console.WriteLine("You are a home-lover.");
+            }
+            else if(userChoise == "dog") 
+            {
+                Console.WriteLine("You are a partygoer.");
+            }
+            else
+            {
+                Console.WriteLine($"You are a {userChoise} friend.");
+            }
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada tema lemmikvärv;'
+            //kui kasutaja sisestab "punane", konsool kuvab "oled romatiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            //kui kasutaja sisestab "roheline", konsool kuvab "oled looduse sõber";
+            //kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik;
+
+            Console.WriteLine("Sisesta oma lemmikvärv:");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch (userColor) 
+            {
+                case "punane": //if(userColor == "punane")
+                    Console.WriteLine("Oled romatiline.");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled loodusesõber.");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} ükssarvik.");
+                    break;
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
